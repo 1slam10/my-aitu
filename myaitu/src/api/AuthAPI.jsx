709b9e52b3a355be4718d253f3c5f095350c31ad
Islam Adilkhan <adilkhanislam9@icloud.com>
2024-05-20 01:21:41 +0500

    feat: add website logo, finished login page

--- myaitu/src/api/AuthAPI.jsx
+++ myaitu/src/api/AuthAPI.jsx
@@ -1,1 +1,10 @@
-export const LoginAPI = () => {};
+import { signInWithEmailAndPassword } from 'firebase/auth'
+import { auth } from '../firebaseConfig';
+
+export const LoginAPI = (email, password) => {
+    try {
+        return signInWithEmailAndPassword(auth, email, password);
+    } catch(err) {
+        alert(err);
+    }
+};
--- myaitu/src/components/LoginComponent.jsx
+++ myaitu/src/components/LoginComponent.jsx
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { LoginAPI } from '../api/AuthAPI';
+import AituLogo from '../assets/AituLogo.png';
+import '../Sass/LoginComponent.scss'
 
 export default function LoginComponent() {
+
+    const [credentials, setCredentials] = useState({});
+
+    const login = async () => {
+        let res = await LoginAPI(credentials.email, credentials.password);
+        console.log(credentials.email, credentials.password)
+        
+        console.log(res?.user);
+    }
+
     return (
-        <div>
-            <h1>LoginComponent</h1>
+        <div className='login-wrapper'>
+            <img src={AituLogo} alt='Aitu Logo' className='aitu-logo'/>
+
+            <div className='login-wrapper-inner'>
+                <h1 className='heading'>Sign in</h1>
+                <p className='subheading'>All Things Uni, All in One Place</p>
+
+                <div className='auth-inputs'>
+                    <input
+                        onChange={(event) => setCredentials({ ...credentials, email: event.target.value })}
+                        type='email'
+                        className='common-input'
+                        placeholder='Email'
+                    />
+
+                    <input
+                        onChange={(event) => setCredentials({ ...credentials, password: event.target.value })}
+                        type='password'
+                        className='common-input'
+                        placeholder='Password'
+                    />
+                </div>
+                <button onClick={login} className='login-btn'>Sign in to MyAITU</button>
+            </div>
         </div>
     )
 }
